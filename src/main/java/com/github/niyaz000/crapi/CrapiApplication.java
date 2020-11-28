49ea5c21327b8fb5499676af2b28723087f1b548
Niyaz K <niyazk2991@gmail.com>
2020-11-28 14:50:55 +0530

    initial commit

---
+++ src/main/java/com/github/niyaz000/crapi/CrapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.niyaz000.crapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/niyaz000/crapi/CrapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.niyaz000.crapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
